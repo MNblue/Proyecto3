14f97019aa322cd185a743b80c5aa871827425cf
Maria <ary023@gmail.com>
2024-01-27 20:14:21 +0100

    footer and nav responsive + logo + favicon

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,25 @@
 import './Footer.css'
 
-function Footer() {
-  
+import logo5 from '../images/logo5.png'
 
-    return (
-      <>
-        <div className='footer'><p>Copyright &copy; 2024 MN</p></div>
-      </>
-    )
-  }
-  
-  export default Footer;
+function Footer() {
+
+
+  return (
+    <>
+      <div className='footer'>
+        <div className='copyR'>
+          <img src={logo5}></img>
+          <p>Copyright &copy; 2024 iMiK Software</p>
+        </div>
+        <div className='textoFooter'>
+          <p>Contacto: 688 999 777</p>
+          <p>Avda. de la tecnología 123, 29001 Málaga</p>
+          <p>contacto@InstitutoSantaTecla.com</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Footer;
